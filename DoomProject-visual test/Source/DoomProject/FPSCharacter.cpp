c03801af8ec1ec9a28f0c50134cc2732d0dc4236
AssortRock <AssortRock@DESKTOP-NVUJ12V>
2023-08-16 12:51:49 +0900

    0816 asc

---
+++ DoomProject-visual test/Source/DoomProject/FPSCharacter.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSCharacter.h"
+
+FPSCharacter::FPSCharacter()
+{
+}
+
+FPSCharacter::~FPSCharacter()
+{
+}
